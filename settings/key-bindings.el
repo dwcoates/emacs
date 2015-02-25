f42a616e69805a593643ce972451ae5ba6219014
Dodge Coates <dcoates91@gmail.com>
2015-02-25 01:51:23 -0500

    cherry-picked some goodies from Wiegley's dot-emacs repo

--- settings/key-bindings.el
+++ settings/key-bindings.el
@@ -1,16 +1,4 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;;;;;;;;;;;;;;;;; KEY MACROS ;;;;;;;;;;;;;;;;;;;;;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;; opens a shell in a new window and places cursor there
-(fset 'open-shell-new-window
-   [?\C-x ?2 ?\C-x ?o ?\M-x ?e ?s ?h ?e ?l ?l return])
-
-(fset 'comment-lisp-line
-   "\C-a;\C-n\C-a")
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;; KEY BINDINGS ;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -21,19 +9,14 @@
 ;; reprioritizes delete-other-windows to its rightful place
 (global-set-key (kbd "C-c 1") 'delete-other-windows)
 
-
 ;; replaces list-buffers binding with ibuffer
 (global-set-key (kbd "C-x C-b") 'ibuffer)
-
 
 ;; delete-region
 (global-set-key (kbd "C-c M-m n") 'apply-macro-to-region-lines)
 
-
-
 ;; iedit
 (global-set-key (kbd "C-;") 'iedit-mode)
-
 
 ;; google c++ style popup
 (global-set-key (kbd "C-c +") 'flymake-popup-current-error-menu)
@@ -44,10 +27,8 @@
   (if arg (other-frame 1) (other-window 1)))
 (global-set-key (kbd "C-\\") 'nispio/other-window)
 
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;;;;;;;;;;;;;;; HELM ;;;;;;;;;;;;;;;;;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; HELM
+;; ----
 (global-set-key (kbd "M-i") 'helm-swoop)
 (global-set-key (kbd "M-I") 'helm-swoop-back-to-last-point)
 (global-set-key (kbd "C-c M-i") 'helm-multi-swoop)
@@ -62,6 +43,126 @@
 (global-set-key (kbd "C-x b") 'helm-mini)
 
 (global-set-key (kbd "C-x C-f") 'helm-find-files)
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;; FUNCTIONS ;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun duplicate-line ()
+  "Duplicate the line containing point."
+  (interactive)
+  (save-excursion
+    (let (line-text)
+      (goto-char (line-beginning-position))
+      (let ((beg (point)))
+        (goto-char (line-end-position))
+        (setq line-text (buffer-substring beg (point))))
+      (if (eobp)
+          (insert ?\n)
+        (forward-line))
+      (open-line 1)
+      (insert line-text))))
+
+(bind-key "C-x C-d" 'duplicate-line)
+(bind-key "C-x C-e" 'pp-eval-last-sexp)
+(bind-key "C-x C-n" 'next-line)
+
+(defun find-alternate-file-with-sudo ()
+  (interactive)
+  (find-alternate-file (concat "/sudo::" (buffer-file-name))))
+
+(bind-key "C-x C-v" 'find-alternate-file-with-sudo)
+
+;;;_  . C-x M-
+
+(bind-key "C-x M-n" 'set-goal-column)
+
+;; inspired by Erik Naggum's `recursive-edit-with-single-window'
+(defmacro recursive-edit-preserving-window-config (body)
+  "*Return a command that enters a recursive edit after executing BODY.
+ Upon exiting the recursive edit (with\\[exit-recursive-edit] (exit)
+ or \\[abort-recursive-edit] (abort)), restore window configuration
+ in current frame."
+  `(lambda ()
+     "See the documentation for `recursive-edit-preserving-window-config'."
+     (interactive)
+     (save-window-excursion
+       ,body
+       (recursive-edit))))
+
+(bind-key "C-c 0"
+  (recursive-edit-preserving-window-config (delete-window)))
+(bind-key "C-c 1"
+  (recursive-edit-preserving-window-config
+   (if (one-window-p 'ignore-minibuffer)
+       (error "Current window is the only window in its frame")
+     (delete-other-windows))))
+
+(bind-key "C-c c" 'compile)
+
+(defun delete-current-line (&optional arg)
+  (interactive "p")
+  (let ((here (point)))
+    (beginning-of-line)
+    (kill-line arg)
+    (goto-char here)))
+
+(bind-key "C-c d" 'delete-current-line)
+
+(bind-key "C-c e E" 'elint-current-buffer)
+
+(defun do-eval-buffer ()
+  (interactive)
+  (call-interactively 'eval-buffer)
+  (message "Buffer has been evaluated"))
+
+(bind-key "C-c e b" 'do-eval-buffer)
+
+(defun goto-line-with-feedback ()
+  "Show line numbers temporarily, while prompting for the line number input"
+  (interactive)
+  (unwind-protect
+      (progn
+        (linum-mode 1)
+        (goto-line (read-number "Goto line: ")))
+    (linum-mode -1)))
+
+(bind-key "C-c g" 'goto-line)
+
+(defun view-clipboard ()
+  (interactive)
+  (delete-other-windows)
+  (switch-to-buffer "*Clipboard*")
+  (let ((inhibit-read-only t))
+    (erase-buffer)
+    (clipboard-yank)
+    (goto-char (point-min))))
+
+(bind-key "C-c V" 'view-clipboard)
+
+(defun delete-to-end-of-buffer ()
+  (interactive)
+  (kill-region (point) (point-max)))
+
+(bind-key "C-c C-z" 'delete-to-end-of-buffer)
+
+
+
+
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;; KEY MACROS ;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; opens a shell in a new window and places cursor there
+(fset 'open-shell-new-window
+   [?\C-x ?2 ?\C-x ?o ?\M-x ?e ?s ?h ?e ?l ?l return])
+
+(fset 'comment-lisp-line
+   "\C-a;\C-n\C-a")
 
 
 
---
+++ settings/setup-keychord.el
@@ -0,0 +1,8 @@
+(require 'key-chord)
+
+
+;; put some chords here when you get around to it. 
+
+(key-chord-mode 1)
+
+(provide 'setup-keychord)
