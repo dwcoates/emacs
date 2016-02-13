8d22f7277241653e73fc3e4c4c103c79118193be
Dodge Coates <dcoates91@gmail.com>
2016-02-12 22:54:40 -0500

    Update basic bindings.

--- key-bindings.el
+++ key-bindings.el
@@ -1,30 +1,57 @@
+;;;;;;;;;;;; NAVIGATION ;;;;;;;;;;;;;;;;
+(defconst my/back-char-key "C-j")
+(defconst my/forward-char-key "C-l")
+(defconst my/prev-line-key "C-k")
+(defconst my/next-line-key "C-i")
+
+(defconst my/beg-line-key "C-o")
+(defconst my/end-line-key "C-p")
+
+(defconst my/isearch-forward-key "C-y")
+(defconst my/isearch-back-key "C-n")
+
+(defconst my/scroll-down-key "C-m")
+(defconst my/scroll-up-key "M-m")
+
+;;;;;;;;;;;;;; EDITING ;;;;;;;;;;;;;;;;;
+(defconst my/kill-line-key "C-a")
+(defconst my/kill-region-key "C-f")
+
+(defconst my/trans-chars-key "C-,")
+(defconst my/trans-words-key "M-,")
+
+(defconst my/yank-key "C-t")
+
+
+;; Wrapped in a progn for testing.
 (progn
   ;;;;;;;;;;;;;;;;;;;;
   ;;;; NAVIGATION ;;;;
   ;;;;;;;;;;;;;;;;;;;;
   ;; DIRECTION
-  (bind-key "C-j" 'backward-char)
-  (bind-key "C-l" 'forward-char)
-  (bind-key "C-k" 'previous-line)
-  (bind-key "C-i" 'next-line)
+  (bind-key my/back-char-key 'backward-char)
+  (bind-key my/forward-char-key 'forward-char)
+  (bind-key my/prev-line-key 'previous-line)
+  (bind-key my/next-line-key 'next-line)
   ;; END/BEG LINE
-  (bind-key "C-o" 'beginning-of-line)
-  (bind-key "C-p" 'end-of-line)
+  (bind-key my/beg-line-key 'beginning-of-line)
+  (bind-key my/end-line-key 'end-of-line)
   ;; SEARCHING
-  (bind-key "C-y" 'isearch-forward)
-  (bind-key "C-n" 'isearch-backward)
+  (bind-key my/isearch-forward-key 'isearch-forward)
+  (bind-key my/isearch-back-key 'isearch-backward)
   ;; SCROLL
-  (bind-key "C-m" 'scroll-down-command)
-  (bind-key "M-m" 'scroll-up-command)
+  (bind-key my/scroll-down-key 'scroll-down-command)
+  (bind-key my/scroll-up-key 'scroll-up-command)
 
-  ;;;;;;;;;;;;;;;;;;;;;;
-  ;;;;   EDITING   ;;;;;
-  ;;;;;;;;;;;;;;;;;;;;;;
+  ;;;;;;;;;;;;;;;;;;;;;
+  ;;;;   EDITING   ;;;;
+  ;;;;;;;;;;;;;;;;;;;;;
   ;; KILL
-  (bind-key "C-a" 'kill-line)
-  (bind-key "C-f" 'kill-region)
+  (bind-key my/kill-line-key 'kill-line)
+  (bind-key my/kill-region-key 'kill-region)
   ;; TRANSPOSE
-  (bind-key "C-" ')
+  (bind-key my/trans-chars-key 'transpose-chars)
+  (bind-key my/trans-words-key 'transpose-words)
   ;; YANK
-  (bind-key "C-f" 'yank)
+  (bind-key my/yank-key 'yank)
   )
