7942f2ca446ccab2e64ffda4a640b98d0f03e952
Dodge Coates <dcoates91@gmail.com>
2016-02-20 16:55:31 -0500

    Finish dwcB-mode. Incomplete default binding.

--- dwcB/bindings.el
+++ dwcB/bindings.el
@@ -1,7 +1,3 @@
-(defconst dwc-major-prefix "C-e")
-(defconst dwc-inter-buffer-prefix "C-w")
-
-
 (keyboard-translate ?\C-i ?\H-i)
 
 
@@ -9,35 +5,27 @@
 ;;;;;;;; KEY GENERICS ;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Direction
-(defconst dwc-backward-key "j")
-(defconst dwc-forward-key "l")
-(defconst dwc-downward-key "k")
-(defconst dwc-upward-key "i")
+(defconst dwcB-backward-key "j")
+(defconst dwcB-forward-key "l")
+(defconst dwcB-downward-key "k")
+(defconst dwcB-upward-key "i")
 ;; Beginning/End
-(defconst dwc-beginning-key "o")
-(defconst dwc-end-key "p")
+(defconst dwcB-beginning-key "o")
+(defconst dwcB-end-key "p")
 ;; Search
-(defconst dwc-search-alpha-key "y")
-(defconst dwc-search-beta-key "n")
+(defconst dwcB-search-alpha-key "y")
+(defconst dwcB-search-beta-key "n")
 ;; Bigger/Smaller
-(defconst dwc-bigger-key ">")
-(defconst dwc-smaller-key "<")
+(defconst dwcB-bigger-key ">")
+(defconst dwcB-smaller-key "<")
 ;; Kill
-(defconst dwc-kill-element-key "d")
-(defconst dwc-kill-big-key "a")
-(defconst dwc-kill-or-save-key "s")
+(defconst dwcB-kill-element-key "d")
+(defconst dwcB-kill-big-key "a")
+(defconst dwcB-kill-or-save-key "s")
 ;; Yank
-(defconst dwc-yank-key "f")
+(defconst dwcB-yank-key "f")
 ;; Transpose
-(defconst dwc-transpose-key "t")
-
-
-
-
-(defvar dwc-global-keymap (make-keymap)
-  "dwc-bindings-mode keymap for inter-buffer and general intra-buffer editing and navigation.")
-
-
+(defconst dwcB-transpose-key "t")
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -48,63 +36,63 @@
 ;;;;;; NAVIGATION ;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Direction
-(define-key dwc-global-keymap (kbd (concat "C-" dwc-backward-key)) 'backward-char)
-(define-key dwc-global-keymap (kbd (concat "C-" dwc-forward-key)) 'forward-char)
-(define-key dwc-global-keymap (kbd (concat "H-" dwc-upward-key)) 'previous-line)
-(define-key dwc-global-keymap (kbd (concat "C-" dwc-downward-key)) 'next-line)
+(define-key dwcB-global-map (kbd (concat "C-" dwcB-backward-key)) 'backward-char)
+(define-key dwcB-global-map (kbd (concat "C-" dwcB-forward-key)) 'forward-char)
+(define-key dwcB-global-map (kbd (concat "H-" dwcB-upward-key)) 'previous-line)
+(define-key dwcB-global-map (kbd (concat "C-" dwcB-downward-key)) 'next-line)
 ;
-(define-key dwc-global-keymap (kbd (concat "M-" dwc-backward-key)) 'backward-word)
-(define-key dwc-global-keymap (kbd (concat "M-" dwc-forward-key)) 'forward-word)
-(define-key dwc-global-keymap (kbd (concat "M-" dwc-downward-key)) 'forward-paragraph)
-(define-key dwc-global-keymap (kbd (concat "M-" dwc-upward-key)) 'backward-paragraph)
+(define-key dwcB-global-map (kbd (concat "M-" dwcB-backward-key)) 'backward-word)
+(define-key dwcB-global-map (kbd (concat "M-" dwcB-forward-key)) 'forward-word)
+(define-key dwcB-global-map (kbd (concat "M-" dwcB-downward-key)) 'forward-paragraph)
+(define-key dwcB-global-map (kbd (concat "M-" dwcB-upward-key)) 'backward-paragraph)
 ;
-(define-key dwc-global-keymap (kbd (concat "C-S-" dwc-forward-key)) 'scroll-left)
-(define-key dwc-global-keymap (kbd (concat "C-S-" dwc-backward-key)) 'scroll-right)
-(define-key dwc-global-keymap (kbd (concat "C-S-" dwc-upward-key)) 'scroll-down-command)
-(define-key dwc-global-keymap (kbd (concat "C-S-" dwc-downward-key)) 'scroll-up-command)
+(define-key dwcB-global-map (kbd (concat "C-S-" dwcB-forward-key)) 'scroll-left)
+(define-key dwcB-global-map (kbd (concat "C-S-" dwcB-backward-key)) 'scroll-right)
+(define-key dwcB-global-map (kbd (concat "C-S-" dwcB-upward-key)) 'scroll-down-command)
+(define-key dwcB-global-map (kbd (concat "C-S-" dwcB-downward-key)) 'scroll-up-command)
 ;; Beginning/End
-(define-key dwc-global-keymap (kbd (concat "C-M-" dwc-upward-key)) 'scroll-other-window-down)
-(define-key dwc-global-keymap (kbd (concat "C-M-" dwc-downward-key)) 'scroll-other-window)
+(define-key dwcB-global-map (kbd (concat "C-M-" dwcB-upward-key)) 'scroll-other-window-down)
+(define-key dwcB-global-map (kbd (concat "C-M-" dwcB-downward-key)) 'scroll-other-window)
 ;
-(define-key dwc-global-keymap (kbd (concat "C-" dwc-beginning-key)) 'beginning-of-line)
-(define-key dwc-global-keymap (kbd (concat "C-" dwc-end-key)) 'end-of-line)
+(define-key dwcB-global-map (kbd (concat "C-" dwcB-beginning-key)) 'beginning-of-line)
+(define-key dwcB-global-map (kbd (concat "C-" dwcB-end-key)) 'end-of-line)
 ;
-(define-key dwc-global-keymap (kbd (concat "M-" dwc-end-key)) 'forward-to-indentation)
-(define-key dwc-global-keymap (kbd (concat "M-" dwc-beginning-key)) 'backward-to-indentation)
+(define-key dwcB-global-map (kbd (concat "M-" dwcB-end-key)) 'forward-to-indentation)
+(define-key dwcB-global-map (kbd (concat "M-" dwcB-beginning-key)) 'backward-to-indentation)
 ;
-(define-key dwc-global-keymap (kbd (concat "C-M-" dwc-beginning-key)) 'beginning-of-buffer)
-(define-key dwc-global-keymap (kbd (concat "C-M-" dwc-end-key)) 'end-of-buffer)
+(define-key dwcB-global-map (kbd (concat "C-M-" dwcB-beginning-key)) 'beginning-of-buffer)
+(define-key dwcB-global-map (kbd (concat "C-M-" dwcB-end-key)) 'end-of-buffer)
 ;; Bigger/Smaller
-(define-key dwc-global-keymap (kbd (concat "C-" dwc-smaller-key)) 'narrow-to-region)
-(define-key dwc-global-keymap (kbd (concat "M-" dwc-smaller-key)) 'narrow-to-page)
-(define-key dwc-global-keymap (kbd (concat "C-" dwc-bigger-key)) 'widen)
+(define-key dwcB-global-map (kbd (concat "C-" dwcB-smaller-key)) 'narrow-to-region)
+(define-key dwcB-global-map (kbd (concat "M-" dwcB-smaller-key)) 'narrow-to-page)
+(define-key dwcB-global-map (kbd (concat "C-" dwcB-bigger-key)) 'widen)
 ;; Search
-(define-key dwc-global-keymap (kbd (concat "C-" dwc-search-alpha-key)) 'isearch-forward)
-(define-key dwc-global-keymap (kbd (concat "C-" dwc-search-beta-key)) 'isearch-backward)
+(define-key dwcB-global-map (kbd (concat "C-" dwcB-search-alpha-key)) 'isearch-forward)
+(define-key dwcB-global-map (kbd (concat "C-" dwcB-search-beta-key)) 'isearch-backward)
 ;; Misc
-(define-key dwc-global-keymap (kbd "C-'") 'recenter-top-bottom)
+(define-key dwcB-global-map (kbd "C-'") 'recenter-top-bottom)
 ;;;;;;;;;;;;;;;;;;;;;
 ;;;;;; EDITING ;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;
 ;; Direction
-(define-key dwc-global-keymap (kbd (concat "C-S-M-" dwc-upward-key)) 'upcase-region)
-(define-key dwc-global-keymap (kbd (concat "C-S-M-" dwc-downward-key)) 'downcase-region)
+(define-key dwcB-global-map (kbd (concat "C-S-M-" dwcB-upward-key)) 'upcase-region)
+(define-key dwcB-global-map (kbd (concat "C-S-M-" dwcB-downward-key)) 'downcase-region)
 ;; Kill
-(define-key dwc-global-keymap (kbd (concat "C-" dwc-kill-big-key)) 'kill-line)
-(define-key dwc-global-keymap (kbd (concat "C-" dwc-kill-or-save-key)) 'kill-region)
+(define-key dwcB-global-map (kbd (concat "C-" dwcB-kill-big-key)) 'kill-line)
+(define-key dwcB-global-map (kbd (concat "C-" dwcB-kill-or-save-key)) 'kill-region)
 ;
-(define-key dwc-global-keymap (kbd (concat "M-" dwc-kill-big-key)) 'kill-sentence)
-(define-key dwc-global-keymap (kbd (concat "M-" dwc-kill-or-save-key)) 'kill-ring-save)
+(define-key dwcB-global-map (kbd (concat "M-" dwcB-kill-big-key)) 'kill-sentence)
+(define-key dwcB-global-map (kbd (concat "M-" dwcB-kill-or-save-key)) 'kill-ring-save)
 ;
-(define-key dwc-global-keymap (kbd "C-z") 'zap-to-char)
+(define-key dwcB-global-map (kbd "C-z") 'zap-to-char)
 ;; Whitespace
-(define-key dwc-global-keymap (kbd "C-v") 'delete-horizontal-space)
-(define-key dwc-global-keymap (kbd "M-v") 'delete-indentation)
+(define-key dwcB-global-map (kbd "C-v") 'delete-horizontal-space)
+(define-key dwcB-global-map (kbd "M-v") 'delete-indentation)
 ;; Transpose
-(define-key dwc-global-keymap (kbd (concat "C-" dwc-transpose-key)) 'transpose-chars)
-(define-key dwc-global-keymap (kbd (concat "M-" dwc-transpose-key)) 'transpose-words)
+(define-key dwcB-global-map (kbd (concat "C-" dwcB-transpose-key)) 'transpose-chars)
+(define-key dwcB-global-map (kbd (concat "M-" dwcB-transpose-key)) 'transpose-words)
 ;; Yank
-(define-key dwc-global-keymap (kbd (concat "C-" dwc-yank-key)) 'yank)
+(define-key dwcB-global-map (kbd (concat "C-" dwcB-yank-key)) 'yank)
 
 
 
@@ -113,58 +101,65 @@
 ;;;;;;;;;;;;;;;;;;;;;; INTER-BUFFER ;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(define-prefix-command 'dwc-inter-buffer-keymap)
-(define-key dwc-global-keymap (kbd dwc-inter-buffer-prefix) 'dwc-inter-buffer-keymap)
 ;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;; NAVIGATION ;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;
-(define-key dwc-inter-buffer-keymap (kbd dwc-forward-key) 'next-buffer)
-(define-key dwc-inter-buffer-keymap (kbd dwc-backward-key) 'previous-buffer)
+(define-key dwcB-inter-buffer-map (kbd dwcB-forward-key) 'next-buffer)
+(define-key dwcB-inter-buffer-map (kbd dwcB-backward-key) 'previous-buffer)
 ;
-(define-key dwc-inter-buffer-keymap (kbd "U") 'switch-to-buffer-other-window)
-(define-key dwc-inter-buffer-keymap (kbd "u") 'switch-to-buffer)
-(define-key dwc-inter-buffer-keymap (kbd "C-u") 'helm-buffers-list)
+(define-key dwcB-inter-buffer-map (kbd "U") 'switch-to-buffer-other-window)
+(define-key dwcB-inter-buffer-map (kbd "u") 'switch-to-buffer)
+(define-key dwcB-inter-buffer-map (kbd "C-u") 'helm-buffers-list)
 ;
-(define-key dwc-inter-buffer-keymap (kbd "o") 'other-frame)
-(define-key dwc-inter-buffer-keymap (kbd "O") 'other-window)
+(define-key dwcB-inter-buffer-map (kbd "o") 'other-frame)
+(define-key dwcB-inter-buffer-map (kbd "O") 'other-window)
 ;; DIRED
-(define-key dwc-inter-buffer-keymap (kbd "/") 'dired-jump)
-(define-key dwc-inter-buffer-keymap (kbd "?") 'dired-jump-other-window)
+(define-key dwcB-inter-buffer-map (kbd "/") 'dired-jump)
+(define-key dwcB-inter-buffer-map (kbd "?") 'dired-jump-other-window)
 ;; FIND FILE
-(define-key dwc-inter-buffer-keymap (kbd dwc-search-alpha-key) 'ido-find-file)
-(define-key dwc-inter-buffer-keymap (kbd (concat "C-" dwc-search-alpha-key)) 'ido-find-file-other-window)
-(define-key dwc-inter-buffer-keymap (kbd (upcase dwc-search-alpha-key)) 'ido-find-file-other-frame)
+(define-key dwcB-inter-buffer-map (kbd dwcB-search-alpha-key) 'ido-find-file)
+(define-key dwcB-inter-buffer-map (kbd (concat "C-" dwcB-search-alpha-key)) 'ido-find-file-other-window)
+(define-key dwcB-inter-buffer-map (kbd (upcase dwcB-search-alpha-key)) 'ido-find-file-other-frame)
 ;;;;;;;;;;;;;;;;;;;;;
 ;;;;;; EDITING ;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;
 ;; RESIZE
-(define-key dwc-inter-buffer-keymap (kbd dwc-bigger-key) 'enlarge-window-horizontally)
-(define-key dwc-inter-buffer-keymap (kbd dwc-smaller-key) 'shrink-window-horizontally)
-(define-key dwc-inter-buffer-keymap (kbd (concat "C-" dwc-bigger-key)) 'enlarge-window)
-(define-key dwc-inter-buffer-keymap (kbd (concat "M-" dwc-smaller-key)) 'shrink-window)
+(define-key dwcB-inter-buffer-map (kbd dwcB-bigger-key) 'enlarge-window-horizontally)
+(define-key dwcB-inter-buffer-map (kbd dwcB-smaller-key) 'shrink-window-horizontally)
+(define-key dwcB-inter-buffer-map (kbd (concat "C-" dwcB-bigger-key)) 'enlarge-window)
+(define-key dwcB-inter-buffer-map (kbd (concat "M-" dwcB-smaller-key)) 'shrink-window)
 ;
-(define-key dwc-inter-buffer-keymap (kbd "b") 'balance-windows)
-(define-key dwc-inter-buffer-keymap (kbd "B") 'shrink-window-if-larger-than-buffer)
-(define-key dwc-inter-buffer-keymap (kbd "v") 'golden-ratio)
-(define-key dwc-inter-buffer-keymap (kbd "V") 'golden-ratio-mode)
+(define-key dwcB-inter-buffer-map (kbd "b") 'balance-windows)
+(define-key dwcB-inter-buffer-map (kbd "B") 'shrink-window-if-larger-than-buffer)
+(define-key dwcB-inter-buffer-map (kbd "v") 'golden-ratio)
+(define-key dwcB-inter-buffer-map (kbd "V") 'golden-ratio-mode)
 ;
-(define-key dwc-inter-buffer-keymap (kbd "z") 'suspend-frame)
+(define-key dwcB-inter-buffer-map (kbd "z") 'suspend-frame)
 ;; SPLIT
-(define-key dwc-inter-buffer-keymap (kbd (upcase dwc-forward-key)) 'split-window-right)
-(define-key dwc-inter-buffer-keymap (kbd (upcase dwc-backward-key)) 'split-horizontally)
-(define-key dwc-inter-buffer-keymap (kbd (upcase dwc-upward-key)) 'split-window-vertically)
-(define-key dwc-inter-buffer-keymap (kbd (upcase dwc-downward-key)) 'split-window-below)
+(define-key dwcB-inter-buffer-map (kbd (upcase dwcB-forward-key)) 'split-window-right)
+(define-key dwcB-inter-buffer-map (kbd (upcase dwcB-backward-key)) 'split-horizontally)
+(define-key dwcB-inter-buffer-map (kbd (upcase dwcB-upward-key)) 'split-window-vertically)
+(define-key dwcB-inter-buffer-map (kbd (upcase dwcB-downward-key)) 'split-window-below)
 ;; KILL
-(define-key dwc-inter-buffer-keymap (kbd dwc-kill-element-key) 'kill-buffer-and-window)
-(define-key dwc-inter-buffer-keymap (kbd (upcase dwc-kill-element-key)) 'kill-some-buffers)
-(define-key dwc-inter-buffer-keymap (kbd (concat "C-" dwc-kill-element-key)) 'kill-buffer)
+(define-key dwcB-inter-buffer-map (kbd dwcB-kill-element-key) 'kill-buffer-and-window)
+(define-key dwcB-inter-buffer-map (kbd (upcase dwcB-kill-element-key)) 'kill-some-buffers)
+(define-key dwcB-inter-buffer-map (kbd (concat "C-" dwcB-kill-element-key)) 'kill-buffer)
 ;
-(define-key dwc-inter-buffer-keymap (kbd (concat "3 " (upcase dwc-kill-element-key))) 'delete-other-frames)
-(define-key dwc-inter-buffer-keymap (kbd (concat "3 " dwc-kill-element-key)) 'delete-frame)
+(define-key dwcB-inter-buffer-map (kbd (concat "3 " (upcase dwcB-kill-element-key))) 'delete-other-frames)
+(define-key dwcB-inter-buffer-map (kbd (concat "3 " dwcB-kill-element-key)) 'delete-frame)
 ;; SAVE
-(define-key dwc-inter-buffer-keymap (kbd (concat "C-" dwc-kill-or-save-key)) 'save-buffer)
-(define-key dwc-inter-buffer-keymap (kbd (upcase dwc-kill-or-save-key)) 'write-file)
-(define-key dwc-inter-buffer-keymap (kbd (concat "M-" dwc-kill-or-save-key)) 'save-some-bueffer)
+(define-key dwcB-inter-buffer-map (kbd (concat "C-" dwcB-kill-or-save-key)) 'save-buffer)
+(define-key dwcB-inter-buffer-map (kbd (upcase dwcB-kill-or-save-key)) 'write-file)
+(define-key dwcB-inter-buffer-map (kbd (concat "M-" dwcB-kill-or-save-key)) 'save-some-bueffer)
+
+
+
+
+
+
+
+
+
 
 
 
@@ -173,40 +168,83 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (use-package smartparens
-  :ensure t)
+   :ensure t)
 
-(if  use-dwc-key-bindings
-  (bind-keys
-   :prefix-map lisp-mode-shared-map
-   :prefix dwc-major-prefix
+
+(setq dwcB-lisp-mode-shared-map (make-sparse-keymap))
+(set-keymap-parent dwcB-lisp-mode-shared-map lisp-mode-shared-map) ;; should be done by dwcB-add-major-map
+
+(setq dwcB-lisp-mode-shared-prefix-map (make-sparse-keymap))
+(define-key dwcB-lisp-mode-shared-map (kbd "C-e") dwcB-lisp-mode-shared-prefix-map)
+
+
    ;; Beg/End
-   (my/beginning-key . sp-beginning-of-sexp)
-   (my/end-key . sp-end-of-sexp)
+(define-key dwcB-lisp-mode-shared-prefix-map
+  (kbd dwcB-beginning-key)  'sp-beginning-of-sexp)
+(define-key dwcB-lisp-mode-shared-prefix-map
+  (kbd dwcB-end-key)  'sp-end-of-sexp)
    ;; Upward/Downward
-   (my/downward-key . sp-down-sexp)
-   (my/upward-key . sp-up-sexp)
-   ((concat "C-" my/downward-key) . sp-backward-down-sexp)
-   ((concat "C-" my/upward-key) . sp-backward-up-sexp)
+(define-key dwcB-lisp-mode-shared-prefix-map
+  (kbd dwcB-downward-key)  'sp-down-sexp)
+(define-key dwcB-lisp-mode-shared-prefix-map
+  (kbd dwcB-upward-key)  'sp-up-sexp)
+(define-key dwcB-lisp-mode-shared-prefix-map
+  (kbd (concat "C-" dwcB-downward-key))  'sp-backward-down-sexp)
+(define-key dwcB-lisp-mode-shared-prefix-map
+  (kbd (concat "C-" dwcB-upward-key))  'sp-backward-up-sexp)
    ;; Forward/Backward
-   (my/forward-key . sp-forward-sexp)
-   (my/backward-key . sp-backward-sexp)
-   ((concat "C-" my/forward-key)  . sp-forward-symbol)
-   ((concat "C-" my/backward-key) . sp-backward-symbol)
-   ((concat "C-" (upcase my/forward-key)) . sp-next-sexp)
-   ((concat "C-" (upcase my/backward-key)) . sp-previous-sexp)
+(define-key dwcB-lisp-mode-shared-prefix-map
+  (kbd dwcB-forward-key)  'sp-forward-sexp)
+(define-key dwcB-lisp-mode-shared-prefix-map
+  (kbd dwcB-backward-key)  'sp-backward-sexp)
+(define-key dwcB-lisp-mode-shared-prefix-map
+  (kbd (concat "C-" dwcB-forward-key))   'sp-forward-symbol)
+(define-key dwcB-lisp-mode-shared-prefix-map
+  (kbd (concat "C-" dwcB-backward-key))  'sp-backward-symbol)
+(define-key dwcB-lisp-mode-shared-prefix-map
+  (kbd (concat "C-" (upcase dwcB-forward-key)))  'sp-next-sexp)
+(define-key dwcB-lisp-mode-shared-prefix-map
+  (kbd (concat "C-" (upcase dwcB-backward-key)))  'sp-previous-sexp)
    ;; Slurp/Barf
-
-   ("e" . sp-forward-slurp-sexp)
-   ("r" . sp-forward-barf-sexp)
-   ("w"  . sp-backward-slurp-sexp)
-   ("q"  . sp-backward-barf-sexp)
-   ;; Transpose
-   (my/transpose-key . sp-transpose-sexp)
-   ((concat "C-" my/transpose-key) . sp-transpose-hybrid-sexp)
+(define-key dwcB-lisp-mode-shared-prefix-map
+  (kbd "e")  'sp-forward-slurp-sexp)
+(define-key dwcB-lisp-mode-shared-prefix-map
+  (kbd "r")  'sp-forward-barf-sexp)
+(define-key dwcB-lisp-mode-shared-prefix-map
+  (kbd "w")   'sp-backward-slurp-sexp)
+(define-key dwcB-lisp-mode-shared-prefix-map
+  (kbd "q")   'sp-backward-barf-sexp)
+;; Transpose
+(define-key dwcB-lisp-mode-shared-prefix-map
+  (kbd dwcB-transpose-key)  'sp-transpose-sexp)
    ;; Kill and Save
-   (my/kill-element-key . sp-kill-sexp)
-   ((concat "C-" my/kill-element-key) . sp-kill-hybrid-sexp)
-   ((concat "C-" (upcase my/kill-element-key)) . sp-backward-kill-sexp)
-   ((concat "M-" my/kill-element-key) . sp-copy-sexp)
-   ;("d" . sp-delete-sexp)        ;; this function snt exist
-   ))
+(define-key dwcB-lisp-mode-shared-prefix-map
+  (kbd dwcB-kill-element-key) 'sp-kill-sexp)
+(define-key dwcB-lisp-mode-shared-prefix-map
+  (kbd (concat "C-" dwcB-kill-element-key)) 'sp-kill-hybrid-sexp)
+(define-key dwcB-lisp-mode-shared-prefix-map
+  (kbd (concat "C-" (upcase dwcB-kill-element-key))) 'sp-backward-kill-sexp)
+(define-key dwcB-lisp-mode-shared-prefix-map
+  (kbd (concat "M-" dwcB-kill-element-key)) 'sp-copy-sexp)
+
+;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;; EMACS LISP ;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;
+
+(setq dwcB-emacs-lisp-mode-map (make-sparse-keymap))
+(set-keymap-parent dwcB-emacs-lisp-mode-map dwcB-lisp-mode-shared-map)
+
+;; (setq dwcB-emacs-lisp-mode-prefix-map (make-sparse-keymap))
+;; (define-key dwcB-emacs-lisp-mode-map (kbd dwcB-major-prefix) dwcB-emacs-lisp-mode-prefix-map)
+
+(dwcB-add-major-map 'emacs-lisp-mode dwcB-emacs-lisp-mode-map)
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;; ORG MODE ;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+
+(provide 'bindings)
--- dwcB/dwc-bindings.el
+++ dwcB/dwc-bindings.el
@@ -1,7 +1,115 @@
+(defconst dwcB-major-prefix "C-e"
+  "dwc-binding prefix used as default for major modes. Major mode commands may be bound to keys outside of prefix (see dwcB-add-major-mode-map).")
+(defconst dwcB-inter-buffer-prefix "C-w"
+  "dwc-binding prefix used as default for inter-buffer navigation and editing.")
 
-(define-minor-mode dwc-bindings-mode
-  "Organized alternative bindings to Emacs default."
+
+(defvar dwcB-global-map (let ((map (make-sparse-keymap))) (set-keymap-parent map global-map) map)
+  "The dwc-bindings global map. Should contain inter- and general intra-buffer binds. Inherits from global-map.")
+
+(defvar dwcB-inter-buffer-map (define-key dwcB-global-map (kbd dwcB-inter-buffer-prefix) (make-sparse-keymap))
+  "dwcB keymap dedicated to inter-buffer editing and navigation. All binds under prefix key dwcB-inter-buffer-prefix")
+
+
+(defvar dwcB-minor-mode-alist '())
+(defvar dwcB-major-mode-alist '())
+
+(define-minor-mode dwcB-mode
+  "Organized alternative bindings to Emacs defaults."
   :lighter " dwcB"
   :global t
-  :keymap dwc-global-keymap
+  (if dwcB-mode
+      (progn
+        (dwcB--set-global-map)                                              ; set global map
+        (mapc 'dwcB-activate-minor-map (mapcar 'car dwcB-minor-mode-alist)) ; set minor maps
+        (add-hook 'after-change-major-mode-hook 'dwcB-update-major-map)
+        (dwcB-update-major-map)
+        )
+    (dwcB--reset-global-map)                                              ; unset global map
+    (mapc 'dwcB-deactivate-minor-map (mapcar 'car dwcB-minor-mode-alist)) ; unset minor maps
+    (remove-hook 'after-change-major-mode-hook 'dwcB-update-major-map)
+    (dwcB-update-major-map)
+    )
   )
+
+
+
+(defun dwcB--reset-global-map ()
+  "Set the current global map to Emacs default, global-map."
+  (if (eq dwcB-global-map (current-global-map))
+      (use-global-map global-map))
+  )
+
+(defun dwcB--set-global-map ()
+  "Set the current global map to dwcB-global-map."
+  (if (eq (current-global-map) global-map)
+      (use-global-map dwcB-global-map))
+  )
+
+
+
+(defun dwcB-add-minor-map (MINOR-MODE KEYMAP &optional inactive)
+  "Add minor mode, MINOR-MODE's, dwcB keymap, KEYMAP, to be used iff the minor mode is active."
+  (if (keymapp KEYMAP)
+      (progn
+        (add-to-list 'dwcB-minor-mode-alist (cons MINOR-MODE KEYMAP))
+         (if (not inactive)
+             (dwcB-activate-minor-binding MINOR-MODE)
+           ))
+    (error "KEYMAP must be a keymap.")
+    )
+  )
+
+(defun dwcB-activate-minor-map (MINOR-MODE)
+  (let ((mode-map (assoc MINOR-MODE dwcB-minor-mode-alist)))
+    (if (not (equal mode-map nil))
+        (add-to-list 'minor-mode-overriding-map-alist mode-map)
+      (message "No dwcB-minor-mode-map for %s; Must be first added with dwcB-add-minor-map"
+              MINOR-MODE)
+      ))
+  )
+
+(defun dwcB-deactivate-minor-map (MINOR-MODE)
+  (let ((mode-map (assoc MINOR-MODE dwcB-minor-mode-alist)))
+    (if (not (equal mode-map nil))
+        (assq-delete-all (car mode-map) minor-mode-overriding-map-alist)
+      (error "Minor mode %s has no dwcB binding."))
+    )
+  )
+
+
+;; check to see if major-mode already has dwcB keymap for it
+;; if it does, replace it.
+;; if it doesnt, add it and record the current major map so that it can be restored
+(defun dwcB-add-major-map (MAJOR-MODE KEYMAP &optional inactive)
+  (let ((mode-map (assoc MAJOR-MODE dwcB-major-mode-alist)))
+    (if (equal mode-map nil)
+        (add-to-list 'dwcB-major-mode-alist (cons MAJOR-MODE (cons (current-local-map) KEYMAP)))
+      (setf (cdr (cdr mode-map)) KEYMAP)
+      )
+    )
+  (if dwcB-mode
+      (dwcB-update-major-map))
+  )
+
+;; receives major-mode, keymap, prefix-map, parent. returns dwcB-keymap created for major-mode
+;; question: how do create parents this way.
+;; (defun dwcB-create-major-map (MAJOR-MODE GENERAL-MAP PREFIX-MAP &optional PARENT)
+;;   ()
+;;   )
+
+(defun dwcB-update-major-map ()
+  "When dwcB-mode is on, switch to corresponding dwcB keymap for the current major mode."
+    (let ((mode-map (assoc major-mode dwcB-major-mode-alist)))
+      (if (not (equal mode-map nil))
+          (if dwcB-mode
+              (use-local-map (cdr (cdr mode-map)))  ; set to major-mode dwcB keymap
+            (use-local-map (car (cdr mode-map)))    ; reset to major-mode original keymap
+            )
+        )
+      )
+    )
+
+(require 'bindings)
+
+(provide 'dwc-bindings)
