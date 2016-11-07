fc7be54ee37f82e849e82b82db25a42736a46580
Dodge W. Coates <dcoates91@gmail.com>
2016-11-06 19:13:32 -0500

    Update agenda appearance

--- custom.el
+++ custom.el
@@ -21,6 +21,7 @@
     ("c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "1af615aabe926740357c6b6c3a611470054efd3bdee28dae6a1bd054d0b43f7d" "7630f2ea026c651a07239948bba7450bc73ec0fa247bc3d123ba4558fab6818a" "422662ca327aec391aaee6e36836af1a489e44b2ead0ad6ad5f22775fa2b0c06" "930a202ae41cb4417a89bc3a6f969ebb7fcea5ffa9df6e7313df4f7a2a631434" "b8c99a002227c98b796605d479bcca6fe71c1c20f22fe68fb9d65b91a9373563" "c87cc60d01cf755375759d165c1d60d9586c6a31f0b5437a0378c2a93cfc8407" default)))
  '(fci-rule-color "#151515")
  '(fringe-mode (quote (4 . 4)) nil (fringe))
+ '(org-agenda-files (quote ("~/personal/everything.org")))
  '(org-default-notes-file "/home/dodge/personal/everything.org")
  '(org-directory "~/personal/")
  '(org-footnote-section nil)
@@ -40,6 +41,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(default ((t (:background nil))))
  '(button ((t (:inherit link))))
  '(company-preview ((t (:background "gray12" :foreground "wheat"))))
  '(company-preview-common ((t (:inherit company-preview :foreground "navajo white"))))
@@ -56,16 +58,13 @@
  '(helm-M-x-key ((t (:foreground "red3" :underline t))))
  '(helm-candidate-number ((t (:background "salmon2" :foreground "black"))))
  '(helm-selection ((t (:background "sienna"))))
-<<<<<<< HEAD
  '(highlight ((t (:background "light goldenrod"))))
-=======
- '(highlight ((t (:background "gray37"))))
  '(linum ((t (:background "DarkGoldenrod4" :foreground "black"))))
->>>>>>> 49e573da5bea5724bd4e4f2c309ae2570a248b98
  '(mode-line ((t (:background "burlywood4" :foreground "gray60" :inverse-video nil :box nil))))
  '(mode-line-buffer-id ((t (:foreground "aquamarine" :weight bold))))
  '(mode-line-highlight ((t (:box (:line-width 2 :color "yellow" :style released-button)))))
  '(mode-line-inactive ((t (:background "gray22" :foreground "gray52" :inverse-video nil :box nil))))
+ '(org-agenda-done ((t (:foreground "olive drab" :weight ultra-light))))
  '(org-block-background ((t (:background "gray17"))))
  '(org-block-begin-line ((t (:foreground "gray14"))) t)
  '(org-block-end-line ((t (:foreground "gray14"))) t)
@@ -75,7 +74,9 @@
  '(org-level-6 ((t (:inherit outline-7))))
  '(org-level-7 ((t (:foreground "yellow green"))))
  '(org-scheduled ((t (:foreground "yellow3"))))
- '(org-scheduled-today ((t (:foreground "yellow2"))))
+ '(org-scheduled-today ((t (:foreground "dark orange" :weight bold))))
+ '(org-upcoming-deadline ((t (:foreground "yellow3"))))
+ '(org-warning ((t (:foreground "red3" :weight bold))))
  '(rainbow-delimiters-depth-1-face ((t (:foreground "indian red"))))
  '(rainbow-delimiters-depth-2-face ((t (:foreground "light sea green"))))
  '(rainbow-delimiters-depth-3-face ((t (:foreground "orchid"))))
