5e834d4758ec190affc81d525844ceecd0e32f87
dwcoates <dodge.w.coates@gmail.com>
2017-02-08 20:25:29 -0500

    rename elpa

---
+++ dependencies/smooth-scroll-20130321.2114/smooth-scroll-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "smooth-scroll" "20130321.2114" "Minor mode for smooth scrolling and in-place scrolling." 'nil :url "http://www.emacswiki.org/emacs/download/smooth-scroll.el" :keywords '("convenience" "emulations" "frames"))
