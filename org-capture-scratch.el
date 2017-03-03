73c4b6127e1ea6c67d662cf6c170f62898c6c35c
dwcoates <dodge.w.coates@gmail.com>
2017-03-03 01:57:12 -0500

    Save

--- org-capture-scratch.el
+++ org-capture-scratch.el
@@ -40,7 +40,7 @@ PROPERTIES and MORE-TAGS are additional optional capture components."
      (concat ":PROPERTIES:\n"
              (mapconcat (lambda (p) (concat ":" (car p) ": " (cadr p))) properties "\n")
              "\n:END:\n"))
-   (when scheduling (concat scheduling "\n"))
+   (when scheduling (if (stringp scheduling) (concat scheduling "\n") "%^{Schedule|SCHEDULE|DEADLINE|}: %T"))
    "\n"
    (when body (concat (if (stringp body) body "\t%?") "\n"))
    (if watermark watermark org-template/meta-data)))
@@ -50,36 +50,36 @@ PROPERTIES and MORE-TAGS are additional optional capture components."
          HD-TASK HD-PRIO HD-PROMPT
          HD-MORE-TAGS SCHEDULING BODY WATERMARK
          PROPERTIES MORE-TAGS)
+  "Stuff SEC GLOBAL-TAGS PREFIX HD-TASK HD-PRIO HD-PROMPT HD-MORE-TAGS.
+SCHEDULING BODY WATERMARK PROPERTIES MORE-TAGS."
 
   (defun w-wrapper (SEC KEYWORD NULL)
+    "This is a SEC KEYWORD NULL."
     (if (plist-member SEC KEYWORD)
         (plist-get SEC KEYWORD)
       NULL))
 
-  (apply
-   'cons
+  (list
    PREFIX
    (plist-get SEC :description)
    (plist-get SEC :location)
-   (list
-    (org-project-template-builder
-     (w-wrapper SEC :headers (org-template/header
-                              HD-TASK
-                              HD-PRIO
-                              HD-PROMPT
-                              HD-MORE-TAGS))
-     (append GLOBAL-TAGS (w-wrapper SEC :scheduling nil))
-     (w-wrapper SEC :scheduling SCHEDULING)
-     (w-wrapper SEC :body BODY)
-     (w-wrapper SEC :watermark WATERMARK)
-     (w-wrapper SEC :properties PROPERTIES)
-     ))
+   (org-project-template-builder
+    (w-wrapper SEC :headers (org-template/header
+                             HD-TASK
+                             HD-PRIO
+                             HD-PROMPT
+                             HD-MORE-TAGS))
+    (append GLOBAL-TAGS (w-wrapper SEC :scheduling nil))
+    (w-wrapper SEC :scheduling SCHEDULING)
+    (w-wrapper SEC :body BODY)
+    (w-wrapper SEC :watermark WATERMARK)
+    (w-wrapper SEC :properties PROPERTIES)
+    )
    (plist-get SEC :keywords)))
 
-(setq basic '(:description "hello, folks!" :location '(id "w293482j-12384985") :scheduling "!!!!!LKJDFI!!!!!!"))
+(setq basic '(:description "hello, folks!" :location '(id "w293482j-12384985") :todo '(:scheduling "!!!!!LKJDFI!!!!!!")))
 (let ((vv (plist-get basic :todo)))
-  (t-wrapper vv "fft" '("sometag" "anothertag") "todo" t nil nil t nil nil nil nil)
-  )
+  (t-wrapper vv '("sometag" "anothertag") "fft" "todo" t nil nil t nil nil nil nil))
 
 
 (defmacro org-make-project-templates (prefix global-tags &rest args)
@@ -90,20 +90,20 @@ PROPERTIES and MORE-TAGS are additional optional capture components."
     (append
      (apply
       'cons
-      (t-wrapper (plist-get basic :todo) prefix global-tags "todo" t nil nil t nil nil nil) ;; tasks
-      (t-wrapper (plist-get basic :idea) prefix global-tags "idea" t nil nil t nil nil nil) ;; idea
-      (t-wrapper (plist-get basic :note) prefix global-tags "note" t nil nil t nil nil nil)) ;; note
+      (t-wrapper (plist-get basic :todo) (concat prefix "t") global-tags "todo" t nil nil t nil nil nil) ;; tasks
+      (t-wrapper (plist-get basic :idea) (concat prefix "i") global-tags "idea" t nil nil t nil nil nil) ;; idea
+      (t-wrapper (plist-get basic :note) (concat prefix "n") global-tags "note" t nil nil t nil nil nil)) ;; note
      (when study
        (apply
         'cons
-        (t-wrapper (plist-get study :question) prefix global-tags "question" t "Question" nil nil nil nil nil) ;; question
-        (t-wrapper (plist-get study :quick-question) prefix global-tags "question" t "Quick Question" nil nil nil nil nil) ;; quick question
-        (t-wrapper (plist-get study :review) prefix global-tags "review" t nil nil t nil nil nil) ;; refresh
-        (t-wrapper (plist-get study :learn) prefix global-tags "learn" t nil nil t nil nil nil))) ;; learn
+        (t-wrapper (plist-get study :question) (concat prefix "U") global-tags "question" t "Question" nil nil nil nil nil) ;; question
+        (t-wrapper (plist-get study :quick-question) (concat prefix "u") global-tags "question" t "Quick Question" nil nil nil nil nil) ;; quick question
+        (t-wrapper (plist-get study :review) (concat prefix "r") global-tags "review" t nil nil t nil nil nil) ;; refresh)
+        (t-wrapper (plist-get study :learn) (concat prefix "l") global-tags "learn" t nil nil t nil nil nil))) ;; learn)
      (when project
        (apply
         'cons
-        (t-wrapper (plist-get project :issue) prefix global-tags "issue" t t nil t nil nil nil) ;; issue
-        (t-wrapper (plist-get project :bug) prefix global-tags "bug" t t nil t nil nil nil) ;; bug
-        (t-wrapper (plist-get project :feature) prefix global-tags "feature" t t nil t nil nil nil)))  ;; feature
+        (t-wrapper (plist-get project :issue) (concat prefix "s") global-tags "issue" t t nil t nil nil nil) ;; issue
+        (t-wrapper (plist-get project :bug) (concat prefix "b") global-tags "bug" t t nil t nil nil nil) ;; bug
+        (t-wrapper (plist-get project :feature) (concat prefix "f") global-tags "feature" t t nil t nil nil nil nil)))  ;; feature
      )))
