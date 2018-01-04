5aed093ee6d637470bacde883eeaef6be5471b86
Dodge W. Coates <dodge.w.coates@gmail.com>
2018-01-04 14:18:19 -0500

    Add custom data (why is this here?).

--- init.el
+++ init.el
@@ -333,3 +333,17 @@
 (let ((post (concat personal-dir ".exclusive/post.el")))
   (when (file-exists-p post)
       (load post)))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-files
+   (quote
+    ("~/personal/projects/" "~/personal/computers/" "~/personal/work/" "~/personal/school/bootcamp/" "~/personal/school/" "~/personal/math/" "~/personal/philosophy/" "~/personal/politics/" "~/personal/history/" "~/personal/"))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(mumamo-background-chunk-major ((((class color) (min-colors 88) (background dark)) nil))))
