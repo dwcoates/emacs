6360354efc841cea8a7fbadb4022ed973cf68307
Dodge W. Coates <dodge.w.coates@gmail.com>
2018-01-21 19:27:14 -0500

    Update install script.

--- setup.sh
+++ setup.sh
@@ -1,3 +1,8 @@
 #! /bin/bash
 
-sudo apt-get install sqlite3
+if [ -f "/etc/debian_version" ];
+   #do stuff
+fi
+
+sudo apt-get install sqlite3 # for helm-dash
+sudo apt-get install texlive-latex-extra # for exporting to pdf in org-mode
