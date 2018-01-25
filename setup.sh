ad8feade8078cde3a7446ffbb1571bbc9e7ad74a
Dodge W. Coates <dodge.w.coates@gmail.com>
2018-01-25 13:38:47 -0500

    Add dependency.

--- setup.sh
+++ setup.sh
@@ -6,3 +6,9 @@ fi
 
 sudo apt-get install sqlite3 # for helm-dash
 sudo apt-get install texlive-latex-extra # for exporting to pdf in org-mode
+
+### Python
+# Dependencies for coding Python in Emacs
+sudo pip install rope
+# Secondary packages
+sudo pip install matplotlib
