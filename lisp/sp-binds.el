e9c0a084efa3c82ff39fcff5a5d27e39268f1214
Dodge W. Coates <dodge.w.coates@gmail.com>
2018-01-31 20:07:57 -0500

    Update sexp bindings.

--- lisp/sp-binds.el
+++ lisp/sp-binds.el
@@ -1,51 +1,52 @@
-(bind-keys
+(bind-keys*
  :map smartparens-mode-map
- ("C-' a" . sp-beginning-of-sexp)
- ("C-' e" . sp-end-of-sexp)
-
- ("C-' k" . sp-down-sexp)
- ("C-' i"   . sp-up-sexp)
- ("C-' j" . sp-backward-down-sexp)
- ("C-' l"   . sp-backward-up-sexp)
-
- ("C-' f" . sp-forward-sexp)
- ("C-' b" . sp-backward-sexp)
-
- ("C-' n" . sp-next-sexp)
- ("C-' p" . sp-previous-sexp)
-
- ("C-' h" . sp-forward-symbol)
- ("C-' g" . sp-backward-symbol)
-
- ("C-' t" . sp-forward-slurp-sexp)
- ("C-' w" . sp-forward-barf-sexp)
- ("C-' r"  . sp-backward-slurp-sexp)
- ("C-' q"  . sp-backward-barf-sexp)
-
- ("C-' C-t" . sp-transpose-sexp)
- ("C-' k" . sp-kill-sexp)
- ("C-' h"   . sp-kill-hybrid-sexp)
- ("C-' C-k"   . sp-backward-kill-sexp)
- ("C-' C-w" . sp-copy-sexp)
-
- ("C-' d" . sp-delete-sexp)        ;; this function doesnt exist?
-
+ ;; delete behavior
  ("<backspace>" . sp-backward-delete-char)
- ("C-<backspace>" . backward-delete-char)     ;; this should be like paredit
- ("M-<backspace>" . sp-backward-kill-word)     ;; this should be like paredit
- ("M-s-<backspace>" . backward-kill-word)     ;; this should be like paredit
- ([remap sp-backward-kill-word] . backward-kill-word)
-
+ ("<C-backspace>" . backward-delete-char)
+ ("<M-backspace>" . sp-backward-kill-word)
+ ("<C-M-backspace>" . backward-kill-word)
+ ;; wrap/unwrap/rewrap
  ("M-[" . sp-backward-unwrap-sexp)
  ("M-]" . sp-unwrap-sexp)
  ("M-s-[" . sp-rewrap-sexp)
-
- ("C-x C-t" . sp-transpose-hybrid-sexp)
-
- ("C-c ("  . wrap-with-parens)
- ("C-c ["  . wrap-with-brackets)
- ("C-c {"  . wrap-with-braces)
- ("C-c '"  . wrap-with-single-quotes)
- ("C-c \"" . wrap-with-double-quotes) ;; This currently causes minor problem with smartparens parser
- ("C-c _"  . wrap-with-underscores)
- ("C-c `"  . wrap-with-back-quotes))
+ :prefix "C-j"
+ :prefix-map smartparens-mode-map
+ ;; wrapping
+ ("("  . (lambda (&optional arg) (interactive "P") (sp-wrap-with-pair "(")))
+ ("["  . (lambda (&optional arg) (interactive "P") (sp-wrap-with-pair "[")))
+ ("{"  . (lambda (&optional arg) (interactive "P") (sp-wrap-with-pair "{")))
+ ("'"  . (lambda (&optional arg) (interactive "P") (sp-wrap-with-pair "'")))
+ ("\"" . (lambda (&optional arg) (interactive "P") (sp-wrap-with-pair "\"")))
+ ("_"  . (lambda (&optional arg) (interactive "P") (sp-wrap-with-pair "_")))
+ ("`"  . (lambda (&optional arg) (interactive "P") (sp-wrap-with-pair "`")))
+ ;; sexp direction
+ ("a" . sp-beginning-of-sexp)
+ ("e" . sp-end-of-sexp)
+ ("i"   . sp-up-sexp)
+ ("j" . sp-backward-down-sexp)
+ ("l"   . sp-backward-up-sexp)
+ ("f" . sp-forward-sexp)
+ ("b" . sp-backward-sexp)
+ ("n" . sp-next-sexp)
+ ("p" . sp-previous-sexp)
+ ;; symbol direction
+ ("h" . sp-forward-symbol)
+ ("g" . sp-backward-symbol)
+ ;; slurping
+ ("t" . sp-forward-slurp-sexp)
+ ("w" . sp-forward-barf-sexp)
+ ("r"  . sp-backward-slurp-sexp)
+ ("q"  . sp-backward-barf-sexp)
+ ;; transposing
+ ("C-t" . sp-transpose-sexp)
+ ("M-t" . sp-transpose-hybrid-sexp)
+ ;; killing/copying
+ ("k" . sp-kill-sexp)
+ ("h"   . sp-kill-hybrid-sexp)
+ ("C-k"   . sp-backward-kill-sexp)
+ ("C-w" . sp-copy-sexp)
+ ;; deleting
+ ("d" . sp-delete-)
+ ("C-d" . sp-delete-sexp)
+ ("<C-backspace>" . sp-backward-delete-symbol)
+ ("<M-backspace>" . sp-backward-kill-sexp))
