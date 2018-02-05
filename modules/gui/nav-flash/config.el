8b1f28ea314313f3d539bfe4f5d3dd6690554846
Dodge W. Coates <dodge.w.coates@gmail.com>
2018-02-05 02:27:57 -0500

    Remove evil stuff from nav stuff.

--- modules/gui/nav-flash/config.el
+++ modules/gui/nav-flash/config.el
@@ -6,13 +6,7 @@
   ;; NOTE In :feature jump `recenter' is hooked to a bunch of jumping commands,
   ;; which will trigger nav-flash.
   (advice-add #'windmove-do-window-select :around #'+doom*blink-cursor-maybe)
-  (advice-add #'recenter :around #'+doom*blink-cursor-maybe)
+  (advice-add #'recenter :around #'+doom*blink-cursor-maybe))
 
-  (after! evil
-    (advice-add #'evil--jumps-jump   :after #'+doom/blink-cursor)
-
-    (advice-add #'evil-window-top    :after #'+doom/blink-cursor)
-    (advice-add #'evil-window-middle :after #'+doom/blink-cursor)
-    (advice-add #'evil-window-bottom :after #'+doom/blink-cursor)))
 
 
