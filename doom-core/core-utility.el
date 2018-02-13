c5811859a8bd51e6759d73b1521cd2b52f7eed95
Dodge W. Coates <dodge.w.coates@gmail.com>
2018-02-13 01:07:13 -0500

    Add core-utility.

---
+++ doom-core/core-utility.el
@@ -0,0 +1,21 @@
+;;; ../doom-core/core-utility.el -*- lexical-binding: t; -*-
+
+(add-hook 'occur-mode-find-occurrence-hook 'recenter)
+
+(map! :map occur-mode-map
+      "<down>" #'occur-step-forward
+      "<up>"   #'occur-step-backward
+      :map global-map
+      "M-o"     #'other-window
+      "C-x C-b" nil
+      "C-x o"   #'other-window
+      "C-x O"   #'other-frame
+      "C-c b"   #'switch-to-other-buffer
+      "C-x M-t" #'transpose-windows
+      "C-x 5 DEL" #'delete-frame
+      "C-x DEL"   #'delete-window
+      :map org-mode-map
+      "C-c o i" nil)
+
+;; (global-unset-key (kbd "C-x 0"))
+;; (global-unset-key (kbd "C-x 5 0"))
