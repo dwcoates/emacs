300f73465beb2117ed5bbd3a962cfc6283135207
Dodge W. Coates <dodge.w.coates@gmail.com>
2018-02-17 02:42:27 -0500

    Fix modeline by uncommenting sml.

--- modules/gui/modeline/config.el
+++ modules/gui/modeline/config.el
@@ -2,25 +2,25 @@
 
 (deftheme dwc-mode-line-theme "Modeline theme.")
 
-;; (def-package! smart-mode-line
-;;  :config
-;;  (setq sml-theme 'dark)
-;;  (sml/setup)
-;;  ;;
-;;  ;; This code breaks because of ws-butler for some reason???
-;;  ;;
-;;  (custom-theme-set-faces
-;;   'dwc-mode-line-theme
-;;   '(mode-line-buffer-id ((t :inherit sml/filename :foreground nil :background nil))) 
-;;   '(mode-line-inactive ((t :foreground "gray60" :background "#404045" :inverse-video nil)))
-;;   '(mode-line     ((t :foreground "gray60" :background "black" :inverse-video nil)))
-;;   '(sml/global    ((t :foreground "gray50" :inverse-video nil)))
-;;   '(sml/modes     ((t :inherit sml/global :foreground "White")))
-;;   '(sml/filename  ((t :inherit sml/global :foreground "#eab700" :weight bold)))
-;;   '(sml/prefix    ((t :inherit sml/global :foreground "#bf6000")))
-;;   '(sml/read-only ((t :inherit sml/not-modified :foreground "DeepSkyBlue")))
-;;   '(persp-selected-face ((t :foreground "ForestGreen" :inherit sml/filename)))
-;;   '(helm-candidate-number ((t :foreground nil :background nil :inherit sml/filename)))))
+(def-package! smart-mode-line
+ :config
+ (setq sml-theme 'dark)
+ (sml/setup)
+ ;;
+ ;; This code breaks because of ws-butler for some reason???
+ ;;
+ (custom-theme-set-faces
+  'dwc-mode-line-theme
+  '(mode-line-buffer-id ((t :inherit sml/filename :foreground nil :background nil)))
+  '(mode-line-inactive ((t :foreground "gray60" :background "#404045" :inverse-video nil)))
+  '(mode-line     ((t :foreground "gray60" :background "black" :inverse-video nil)))
+  '(sml/global    ((t :foreground "gray50" :inverse-video nil)))
+  '(sml/modes     ((t :inherit sml/global :foreground "White")))
+  '(sml/filename  ((t :inherit sml/global :foreground "#eab700" :weight bold)))
+  '(sml/prefix    ((t :inherit sml/global :foreground "#bf6000")))
+  '(sml/read-only ((t :inherit sml/not-modified :foreground "DeepSkyBlue")))
+  '(persp-selected-face ((t :foreground "ForestGreen" :inherit sml/filename)))
+  '(helm-candidate-number ((t :foreground nil :background nil :inherit sml/filename)))))
 
 ;; ;;;###autoload
 ;; (when load-file-name
