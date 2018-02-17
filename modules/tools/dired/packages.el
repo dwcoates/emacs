429845775f1a18b5176cebea60f80012121dc80d
Dodge W. Coates <dodge.w.coates@gmail.com>
2018-02-17 01:32:33 -0500

    Comment out dired-details def-package.

--- modules/tools/dired/packages.el
+++ modules/tools/dired/packages.el
@@ -2,4 +2,5 @@
 ;;; emacs/dired/packages.el
 
 (package! dired-k)
-
+(package! dired-subtree)
+;; (package! dired-details)   ; doesn't work right now. Prob because melpa no longer support emacs wiki
