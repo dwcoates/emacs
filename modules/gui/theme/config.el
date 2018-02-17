c9da5715ffcd6222b29757e08d5352a52d35c566
Dodge W. Coates <dodge.w.coates@gmail.com>
2018-02-17 02:24:32 -0500

    Properly grab atchka with quelpa.

--- modules/gui/theme/config.el
+++ modules/gui/theme/config.el
@@ -1,7 +1,3 @@
 ;;; gui/theme/config.el -*- lexical-binding: t; -*-
 
-(add-to-list 'load-path (concat doom-lisp-dir "atchka"))
-
-(require 'atchka-theme)
-
-(add-hook 'after-init-hook (lambda () (load-theme 'atchka t)))
+(def-package! atchka-theme)
---
+++ modules/gui/theme/packages.el
@@ -0,0 +1,7 @@
+;;; gui/theme/package.el -*- lexical-binding: t; -*-
+
+(package! atchka-theme
+  :recipe (:fetcher github
+           :repo "dwcoates/atchka"
+           :files ("*")))
+
