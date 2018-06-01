33a03fc17d2f7f53189c56790cea933b1c821fde
Dodge W. Coates <dodge@blackbox.home>
2018-06-01 14:03:49 -0400

    Add workspace bindings.

--- modules/personal/basic/bindings.el
+++ modules/personal/basic/bindings.el
@@ -18,6 +18,33 @@
       "e" #'previous-error
       "h" #'smart-backward)
 
+(unbind-key "C-z" global-map)
+
+(map!
+ (:desc "workspace" :prefix "C-z"
+   :desc "Display tab bar"          "TAB" #'+workspace/display
+   :desc "Cycle to next workspace"  "n"   #'+workspace/cycle
+   :desc "Create new workspace"     "c"   #'+workspace/new
+   :desc "Load workspace from file" "l"   #'+workspace/load
+   :desc "Load last session"        "L"   (λ! (+workspace/load-session))
+   :desc "Save workspace to file"   "s"   #'+workspace/save
+   :desc "Autosave current session" "S"   #'+workspace/save-session
+   :desc "Switch workspace"         "."   #'+workspace/switch-to
+   :desc "Delete session"           "X"   #'+workspace/kill-session
+   :desc "Delete this workspace"    "d"   #'+workspace/delete
+   :desc "Load session"             "L"   #'+workspace/load-session
+   :desc "Next workspace"           "]"   #'+workspace/switch-right
+   :desc "Previous workspace"       "["   #'+workspace/switch-left
+   :desc "Switch to 1st workspace"  "1"   (λ! (+workspace/switch-to 0))
+   :desc "Switch to 2nd workspace"  "2"   (λ! (+workspace/switch-to 1))
+   :desc "Switch to 3rd workspace"  "3"   (λ! (+workspace/switch-to 2))
+   :desc "Switch to 4th workspace"  "4"   (λ! (+workspace/switch-to 3))
+   :desc "Switch to 5th workspace"  "5"   (λ! (+workspace/switch-to 4))
+   :desc "Switch to 6th workspace"  "6"   (λ! (+workspace/switch-to 5))
+   :desc "Switch to 7th workspace"  "7"   (λ! (+workspace/switch-to 6))
+   :desc "Switch to 8th workspace"  "8"   (λ! (+workspace/switch-to 7))
+   :desc "Switch to 9th workspace"  "9"   (λ! (+workspace/switch-to 8))))
+
 ;; (map! 
 ;;       [remap find-tag]         #'projectile-find-tag
 
