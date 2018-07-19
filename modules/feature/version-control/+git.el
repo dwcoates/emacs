e5310196b30b2f6fc08ee9d5f35d44d25fb573ef
Dodge W. Coates <dodge.w.coates@gmail.com>
2018-07-19 14:10:55 -0400

    Don't split windows with magit.

--- modules/feature/version-control/+git.el
+++ modules/feature/version-control/+git.el
@@ -82,6 +82,7 @@
   :commands (magit-status magit-blame)
   :config
   (set! :evil-state 'magit-status-mode 'emacs)
+  (set! :popup "^\\*magit.+\\*$" :regexp t :same t)
   (after! evil
     ;; Switch to emacs state only while in `magit-blame-mode', then back when
     ;; its done (since it's a minor-mode).
